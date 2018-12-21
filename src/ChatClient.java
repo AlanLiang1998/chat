ae7e62095375ed9bb74377618b81714ad44bb8c9
AlanLiang <1114173646@qq.com>
2018-12-21 23:25:25 +0800

    Client发送一条信息到Server

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -4,12 +4,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
 public class ChatClient extends Frame {
     TextField tf = new TextField();
     TextArea ta = new TextArea();
+    Socket s = null;
 
     public void launchFrame() {
         setTitle("chat");
@@ -31,19 +33,30 @@ public class ChatClient extends Frame {
     private class TFListener implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
-            String s = tf.getText().trim();
-            ta.setText(s);
+            String str = tf.getText().trim();
+            ta.setText(str);
+            tf.setText("");
+            try {
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                dos.writeUTF(str);
+                dos.flush();
+                dos.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
         }
     }
 
     public void connect() {
         try {
-            Socket s = new Socket("192.168.203.1", 8813);
+            s = new Socket("192.168.203.1", 8813);
             System.out.println("connected");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,3 +1,4 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,7 +9,11 @@ public class ChatServer {
             ServerSocket ss = new ServerSocket(8813);
             while (true) {
                 Socket s = ss.accept();
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                String str = dis.readUTF();
+                System.out.println(str);
                 System.out.println("A client connected!");
+                dis.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
